b51accde3f658d0c08e94495b6a7dc733f03099e
Dupeyrat Kevin <kevin.dupeyrat@gmail.com>
2020-12-30 00:54:24 +0100

    Add test config

---
+++ src/test/java/com/test/batch/ConfigurationForTest.java
@@ -0,0 +1,9 @@
+package com.test.batch;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.test.batch")
+public class ConfigurationForTest {
+}
---
+++ src/test/java/com/test/batch/step/ChargementFormateurStepConfigTest.java
@@ -0,0 +1,14 @@
+package com.test.batch.step;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChargementFormateurStepConfigTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
